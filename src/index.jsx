c04c8b9a9d27a7692aa9278e22a4c32d898fb357
John Zeiders <zeiders@usc.edu>
2018-03-17 11:45:27 +0100

    Fixes

--- src/index.jsx
+++ src/index.jsx
@@ -3,17 +3,16 @@ import ReactDOM from "react-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import environmentReducer from "./reducers/environmentReducer";
-import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
 
 const store = createStore(environmentReducer);
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  // eslint-disable-next-line no-undef
-  document.querySelector("#root")
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	// eslint-disable-next-line no-undef
+	document.querySelector("#root")
 );
 /**
 module.exports = {
