d3732bbbd4345ff3969f9203ee7b377e5af97da7
AmineAfia <afia.amine@gmail.com>
2018-03-17 22:27:06 +0100

    #

--- src/containers/DoctorContainer.jsx
+++ src/containers/DoctorContainer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import DoctorView from "../components/content/DoctorView";
 import { AppConsumer } from "../App";
+import { setTimeout } from "timers";
 
 export default class DoctorContainer extends React.Component {
   constructor() {
@@ -18,9 +19,7 @@ export default class DoctorContainer extends React.Component {
           if (context && !this.state.name) {
             context.doctors(1).then(doc => {
               if (doc[1]) {
-                this.setState({
-                  name: doc[1]
-                });
+                setTimeout(this.setState({ name: doc[1] }), 1);
               }
             });
           }
