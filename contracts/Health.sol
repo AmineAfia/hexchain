d574a2d505c4c8f9c7aa3e3714875f3089e8cb8f
AmineAfia <afia.amine@gmail.com>
2018-03-17 17:34:25 +0100

    Adds lookup patient from the doctors side

--- contracts/Health.sol
+++ contracts/Health.sol
@@ -110,6 +110,29 @@ contract Health {
     }
 
     // lookupPatient
+    function getPatientByName(string _name) public returns(string[3]) {
+        for (uint x = 0; x <= patientsCount; x++) {
+            if (keccak256(patients[x].name) == keccak256(_name)) {
+                return [patients[x].name, uintToString(patients[x].age), patients[x].country];
+            }
+        }
+    }
+    
+    function uintToString(uint v) public constant returns (string str) {
+        uint maxlength = 100;
+        bytes memory reversed = new bytes(maxlength);
+        uint i = 0;
+        while (v != 0) {
+            uint remainder = v % 10;
+            v = v / 10;
+            reversed[i++] = byte(48 + remainder);
+        }
+        bytes memory s = new bytes(i + 1);
+        for (uint j = 0; j <= i; j++) {
+            s[j] = reversed[i - j];
+        }
+        str = string(s);
+    }
 }
 
 
