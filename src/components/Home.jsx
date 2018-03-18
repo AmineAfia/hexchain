efc65c35b5b2ff2a21e2d7f073f1c08c1d825de6
fthamm <ft.hamm@gmx.de>
2018-03-18 06:59:52 +0100

    Home.jsx directory and new institute.svg

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { Component } from "react";
+import { Redirect } from "react-router";
+import "./Home.scss";
 
-const Home = ()=>{
-    return <div/>
+export default class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      redirect: false
+    };
+  }
+  render() {
+    const clickedDoctor = event => {
+      this.setState({ redirect: "doctor" });
+    };
+    const clickedInstitute = event => {
+      this.setState({ redirect: "institute" });
+    };
+
+    if (this.state.redirect == false) {
+      return (
+        <div style={{ height: "100%", width: "100%" }}>
+          <font size="12" style={{ width: "100%" }}>
+            I am...
+          </font>
+          <button onClick={clickedDoctor}>
+            <img src="../../img/doctor.svg" />
+            Doctor
+          </button>
+          <button onClick={clickedInstitute}>
+            <img src="../../img/institute.svg" width="100%" />
+            Institute
+          </button>
+        </div>
+      );
+    } else {
+      if (this.state.redirect == "doctor") {
+        this.setState({ redirect: true });
+        return <Redirect to="/doctor" push />;
+      } else {
+        this.setState({ redirect: true });
+        return <Redirect to="/orgasearch" push />;
+      }
+    }
+  }
 }
-
-export default Home;
