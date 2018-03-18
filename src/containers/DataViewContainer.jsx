33de1a405bc1b00f19d923174bc229b4b6dc72d4
John Zeiders <zeiders@usc.edu>
2018-03-18 02:25:21 +0100

    Styling plus

--- src/containers/DataViewContainer.jsx
+++ src/containers/DataViewContainer.jsx
@@ -2,21 +2,24 @@ import * as React from "react";
 import DataView from "../components/content/DataView";
 
 export default class DataViewContainer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-    this.setActivePrice = this.setActivePrice.bind(this);
-  }
-  setActivePrice(value) {
-    this.setState({ activePrice: value });
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			activePrice: 0
+		};
+		this.setActivePrice = this.setActivePrice.bind(this);
+	}
+	setActivePrice(value) {
+		this.setState({ activePrice: value });
+	}
 
-  render() {
-    return (
-      <DataView
-        setActivePrice={this.setActivePrice}
-        activePrice={this.state.activePrice}
-      />
-    );
-  }
+	render() {
+		return (
+			<DataView
+				setActivePrice={this.setActivePrice}
+				activePrice={this.state.activePrice}
+				prices={[2.3, 4, 7.5]}
+			/>
+		);
+	}
 }
